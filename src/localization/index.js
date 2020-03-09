d96f0cc0078ec030ed1de32db281de983a924d21
M4lfr1d <M4lfr1d@yandex.com>
2020-03-09 17:32:14 +0400

    Implement I18n #23 WIP

--- src/localization/index.js
+++ src/localization/index.js
@@ -1,12 +1,36 @@
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
-import en from './translations/en/en.json';
-import ru from './translations/ru/ru.json';
+import {
+  getTranslationsFromScene,
+  getLanguages,
+  generateTranslationsObject,
+} from '../helpers/TranslationsHelper';
 
 i18n.locale = Localization.locale;
 i18n.fallbacks = true;
 
-i18n.translations = {
-  en,
-  ru,
+const collectSceneTranslations = scene => {
+  const everyTranslation = getTranslationsFromScene(scene);
+  const allLanguages = getLanguages(everyTranslation);
+  const translations = generateTranslationsObject(
+    allLanguages,
+    everyTranslation,
+  );
+  i18n.translations = translations;
+};
+
+const mutateObjectProperty = (lasti18nindex, obj) =>
+  obj.constructor === Object &&
+  Object.keys(obj).forEach(key => {
+    const nexti18nindex = `${lasti18nindex}_${key}`;
+    if (obj[key].translations !== undefined) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = i18n.t[`${nexti18nindex}`];
+    }
+    mutateObjectProperty(nexti18nindex, obj[key]);
+  });
+
+export const internationalizeScene = scene => {
+  collectSceneTranslations(scene);
+  mutateObjectProperty('SCENES', scene);
 };
