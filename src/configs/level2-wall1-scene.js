8833aac64d92ecc3e5a6265e1b05ffbbef891ba1
Mikayel Saghyan <xxmuaddib@gmail.com>
2020-06-19 21:20:28 +0400

    minor fix

--- src/configs/level2-wall1-scene.js
+++ src/configs/level2-wall1-scene.js
@@ -395,6 +395,11 @@ export const LEVEL2_WALL1_SCENE = {
                     en: 'Ok!',
                   },
                 },
+                character: {
+                  translations: {
+                    en: 'Ok!',
+                  },
+                },
                 drop: true,
                 heroElement: {
                   bg: 'rgba(255, 255, 255, .8)',
